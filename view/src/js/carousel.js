973b68792cdc88260e6af52d718408ad45e2539f
Vitoria Fortes <fortesvitoria@gmail.com>
2025-11-18 10:14:17 -0300

    atualiza card bicicletas

--- view/src/js/carousel.js
+++ view/src/js/carousel.js
@@ -1,31 +1,51 @@
 // carousel info-bike
-$(document).ready(function(){
-    $('.carousel-info-bike').slick();
-    
+$(document).ready(function () {
+  $('.carousel-info-bike').slick();
+
+});
+
+//carousel-bikes
+
+$(document).ready(function () {
+  $('.carousel-bikes').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+
+    responsive: [
+      {
+        breakpoint: 860,  
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 
-  //carousel-bikes
+});
 
-  $(document).ready(function(){
-    $('.carousel-bikes').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2000,
-    });
-  
+//carousel-roupas
+
+$(document).ready(function () {
+  $('.carousel-roupas').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+
+    responsive: [
+      {
+        breakpoint: 860, 
+        settings: {
+          slidesToShow: 1, 
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 
-    //carousel-roupas
-
-    $(document).ready(function(){
-      $('.carousel-roupas').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-      });
-    
-    });
+});
 
 
