ddb8bc3132d02b26d98cd07496492ed89a9363b4
Vitoria Fortes <fortesvitoria@gmail.com>
2025-11-26 09:02:26 -0300

    ajusta css tabela

--- view/paginas/admin.php
+++ view/paginas/admin.php
@@ -99,35 +99,41 @@ inserirProdutos($connect);
                 </div>
 
                 <div class="bloco">
-                    <?php
-                    $tabela = "usuarios";
-                    $order = "nome";
-                    $usuarios = buscaTodosDados($connect, $tabela, 1, $order);
-                    ?>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Nome:</th>
-                                <th>Sobrenome:</th>
-                                <th>E-mail:</th>
-                                <th>Nascimento:</th>
-
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            foreach ($usuarios as $usuario) : ?>
-                            <tr>
-                                <td><?php echo $usuario['nome']; ?></td>
-                                <td><?php echo $usuario['sobrenome']; ?></td>
-                                <td><?php echo $usuario['email']; ?></td>
-                                <td><?php echo $usuario['dt_nascimento']; ?></td>
-                            </tr>
-                            <?php
-                            endforeach;
-                            ?>
-                        </tbody>
-                    </table>
+                    <h3>Usuários cadastrados: </h3>
+                    <div class="tabela">
+                        
+                        <?php
+                        $tabela = "usuarios";
+                        $order = "nome";
+                        $usuarios = buscaTodosDados($connect, $tabela, 1, $order);
+                        ?>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Nome:</th>
+                                    <th>Sobrenome:</th>
+                                    <th>E-mail:</th>
+                                    <th>Nascimento:</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                #mostra tabela somente de usuarios que não são admins
+                                foreach ($usuarios as $usuario) {
+                                    if ($usuario['is_admin'] != 1) { ?>
+                                        <tr>
+                                            <td><?php echo $usuario['nome']; ?></td>
+                                            <td><?php echo $usuario['sobrenome']; ?></td>
+                                            <td><?php echo $usuario['email']; ?></td>
+                                            <td><?php echo $usuario['dt_nascimento']; ?></td>
+                                        </tr>
+                                <?php
+                                    };
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
 
                 <!-- INICIO FORMULARIO -->
